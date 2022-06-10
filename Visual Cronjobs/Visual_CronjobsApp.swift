0812260a7bea5e3c5de22b1029677079275b953b
Wafarm <3255458795@qq.com>
2022-06-10 11:44:31 +0800

    Initial Commit

---
+++ Visual Cronjobs/Visual_CronjobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_CronjobsApp.swift
+//  Visual Cronjobs
+//
+//  Created by qinyuyao on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Visual_CronjobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
